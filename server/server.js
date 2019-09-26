c42561d13dff2165902ade2bdd0103370af6a1dd
Brian Lowe <mynameisboy@gmail.com>
2019-09-26 11:23:25 -0600

    form skeleton in header

--- server/server.js
+++ server/server.js
@@ -9,3 +9,7 @@ const {SERVER_PORT, CONNECTION_STRING} = process.env
 
 
 massive(CONNECTION_STRING).then()
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Listening on port ${SERVER_PORT}`)
+})
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import john from '../../assets/john_sombrero.png'
+import './header.css'
 
 class Header extends Component {
   constructor() {
@@ -11,7 +13,15 @@ class Header extends Component {
   render() {
       return(
           <div className="header">
-              Header
+              <div className="logo">
+                <img src={john} alt=""/>
+                <h1>Comprajuegos</h1>
+              </div>
+              <div className="login-form">
+                <input placeholder='Email' type="text"/>
+                <input placeholder='Password' type="password"/>
+                <button className="login">Login</button>
+              </div>
           </div>
       )
   }
